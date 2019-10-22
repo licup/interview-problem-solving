f3136b43ca2caa1772c31d26ee11c5b1de51df0f
licup <41974174+licup@users.noreply.github.com>
2019-10-22 08:41:35 -0700

    Module 2 Practice Problem #2

--- excelColumnNum.py
+++ excelColumnNum.py
@@ -1,9 +1,10 @@
-def excel_column_to_number(n):
+def excel_column_to_number(n): #Takes in a string input and converts it to the the excel column number
   letter = 0
   for current_letter in n.upper():
     letter = 26 * letter + (ord(current_letter) - 65 + 1)
   return letter
-#Test Cases
 
+#Test Cases
 print(excel_column_to_number("A")) #returns 1
 print(excel_column_to_number("AZ")) #returns 52
+print(excel_column_to_number("BZ")) #returns 78
