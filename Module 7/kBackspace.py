779113691f91f9b96432b3a4858981aeb12ac290
Edgar Licup <41974174+licup@users.noreply.github.com>
2019-11-13 09:46:04 -0800

    Practice Problem

--- Module 7/kBackspace.py
+++ Module 7/kBackspace.py
@@ -1,3 +1,17 @@
+'''
+K Backspaces
+The backspace key is broken. Every time the backspace key is pressed, instead of deleting the last 
+(non-backspace) character, a '<' is entered. 
+
+Given a string typed with the broken backspace key, write a program that outputs the 
+intended string i.e what the keyboard output should be when the backspace key works properly.
+
+Input
+One line containing the string that was written in the text editor. 
+Only contains lowercase letters from the English alphabet as well as the character '<'.
+'<' will not be the first character.
+'''
+
 def k_backspace(inputString):
   stack = []
   for character in inputString:
